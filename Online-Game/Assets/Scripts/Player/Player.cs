5d0cdd126f5a20e29a2bb627b096cd4e9364f438
Guitar <wachsirin.sara@gmail.com>
2024-02-13 17:24:27 +0700

    Guy - Character Walk Animation

--- Online-Game/Assets/Scripts/Player/Player.cs
+++ Online-Game/Assets/Scripts/Player/Player.cs
@@ -20,10 +20,6 @@ public class Player : NetworkBehaviour
     [SerializeField] private TMP_Text currentHpTextOverHead;
     [SerializeField] private TMP_Text nameTextOverHead;
 
-    [Header("UI Reference")]
-    [SerializeField] private Image hpBar; // The problem is hpBar becomes owned by the last player join
-    [SerializeField] private TMP_Text currentHpText; // The problem is hpBar becomes owned by the last player join
-
     private bool isDead;
     private readonly int statsConvert = 10;
     private readonly float lerpSpeed = 3f;
@@ -35,7 +31,6 @@ public class Player : NetworkBehaviour
         {
             maxHp.Value = PlayerVit * statsConvert;
             hp.Value = maxHp.Value;
-            TryReference();
         }
     }
 
@@ -52,18 +47,15 @@ public class Player : NetworkBehaviour
         // Update HpBar Color
         Color hpBarColor = Color.Lerp(redFF5858, greenCFFF57, (float)hp.Value / maxHp.Value);
         hpBarOverHead.color = hpBarColor;
-        hpBar.color = hpBarColor;
 
         #region OverHead & Screen UI
         // Update UI Bar
         float targetFillAmount = (float)hp.Value / maxHp.Value;
         hpBarOverHead.fillAmount = Mathf.Lerp(hpBarOverHead.fillAmount, targetFillAmount, lerpSpeed * Time.deltaTime);
         hpBarOverHead.fillAmount = Mathf.Clamp01(hpBarOverHead.fillAmount);
-        hpBar.fillAmount = hpBarOverHead.fillAmount;
 
         // Update UI Text
         currentHpTextOverHead.text = hp.Value + "/" + maxHp.Value;
-        currentHpText.text = currentHpTextOverHead.text;
         #endregion
     }
 
@@ -89,27 +81,6 @@ public class Player : NetworkBehaviour
         {
             hp.Value = 0;
             isDead = true;
-        }
-    }
-    #endregion
-
-    #region Try to reference game object
-    private void TryReference()
-    {
-        // Find the GameObject named "HpBar" under the current GameObject
-        GameObject hpBarGameObject = GameObject.Find("HpBar");
-        GameObject currentHpTextGameObject = GameObject.Find("CurrentHpText");
-
-        // Check if GameObject is found.
-        if (hpBarGameObject && currentHpTextGameObject != null)
-        {
-            // Get the Image component from the found GameObject
-            hpBar = hpBarGameObject.GetComponent<Image>();
-            currentHpText = currentHpTextGameObject.GetComponent<TMP_Text>();
-        }
-        else
-        {
-            Debug.LogWarning("The GameObject was not found.");
         }
     }
     #endregion
