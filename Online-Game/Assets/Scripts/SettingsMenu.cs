96a01299522952aa86119a9c485eb5ef0bec17cd
Guitar <wachsirin.sara@gmail.com>
2024-04-01 21:59:53 +0700

    Add SettingsMenu : Resolution Dropdown

---
+++ Online-Game/Assets/Scripts/SettingsMenu.cs
@@ -0,0 +1,43 @@
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class SettingsMenu : MonoBehaviour
+{
+    public TMP_Dropdown resolutionDropdown;
+    private Resolution[] resolutions;
+
+    [System.Obsolete]
+    private void Start()
+    {
+        resolutions = Screen.resolutions;
+
+        resolutionDropdown.ClearOptions();
+
+        List<string> options = new();
+
+        int currentResolutionIndex = 0;
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + " x " + resolutions[i].height + " @" + resolutions[i].refreshRate + "hz";
+            options.Add(option);
+
+            if (resolutions[i].width == Screen.width &&
+                resolutions[i].height == Screen.height &&
+                resolutions[i].refreshRate == Screen.currentResolution.refreshRate)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+    }
+
+    public void SetResolution()
+    {
+        Resolution resolution = resolutions[resolutionDropdown.value];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
+}
