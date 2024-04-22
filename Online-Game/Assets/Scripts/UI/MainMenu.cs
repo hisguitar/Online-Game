f2df402d967cbd3e8d3ecb72f73278e5440076c1
Guitar <wachsirin.sara@gmail.com>
2024-04-22 16:03:43 +0700

    Add detect 'JoinCode'

--- Online-Game/Assets/Scripts/UI/MainMenu.cs
+++ Online-Game/Assets/Scripts/UI/MainMenu.cs
@@ -1,9 +1,18 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
     [SerializeField] private TMP_InputField joinCodeField;
+    [SerializeField] private Button clientButton;
+    [SerializeField] private int minNameLength = 1;
+    [SerializeField] private int maxNameLength = 12;
+
+    private void Update()
+    {
+        HandleCodeChanged();
+    }
 
     public async void StartHost()
     {
@@ -13,5 +22,13 @@ public class MainMenu : MonoBehaviour
     public async void StartClient()
     {
         await ClientSingleton.Instance.GameManager.StartClientAsync(joinCodeField.text);
+    }
+
+    // Check text.Length in joinCodeField
+    public void HandleCodeChanged()
+    {
+        clientButton.interactable =
+            joinCodeField.text.Length >= minNameLength &&
+            joinCodeField.text.Length <= maxNameLength;
     }
 }
