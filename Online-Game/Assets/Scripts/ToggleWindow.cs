1fe15cdeffbb196cae82349a1f64f99905c37aa0
Guitar <wachsirin.sara@gmail.com>
2024-05-04 00:46:32 +0700

    Update UI,  Add shortcut to toggle window

--- Online-Game/Assets/Scripts/ToggleWindow.cs
+++ Online-Game/Assets/Scripts/ToggleWindow.cs
@@ -2,25 +2,40 @@ using UnityEngine;
 
 public class ToggleWindow : MonoBehaviour
 {
-    // Add this script to any window you need to open/close, Can be use to button.
-    public void OnOpenOrClose()
+    public GameObject window;
+    public KeyCode shortcut; // Key to open/close window
+
+    private void Update()
     {
-        if (gameObject.activeSelf == false)
+        // Shortcut to open/close window
+        if (Input.GetKeyDown(shortcut))
         {
-            LeanTween.scale(gameObject, new Vector3(1, 1, 1), 0.25f).setOnStart(Toggle);
-        }
-        else if (gameObject.activeSelf == true)
-        {
-            LeanTween.scale(gameObject, new Vector3(0, 0, 0), 0.25f).setOnComplete(Toggle);
+            OnOpenOrClose();
         }
     }
 
+    #region window control
+    // Add this script to any window you need to open/close, Can be use to button.
+    public void OnOpenOrClose()
+    {
+        if (window.activeSelf == false)
+        {
+            LeanTween.scale(window, new Vector3(1, 1, 1), 0.25f).setOnStart(Toggle);
+        }
+        else if (window.activeSelf == true)
+        {
+            LeanTween.scale(window, new Vector3(0, 0, 0), 0.25f).setOnComplete(Toggle);
+        }
+    }
+
+    // This method below already used in OnOpenOrClose method
     // Toggle window active/inactive
     private void Toggle()
     {
-        if (gameObject != null)
+        if (window != null)
         {
-            gameObject.SetActive(!gameObject.activeSelf);
+            window.SetActive(!window.activeSelf);
         }
     }
+    #endregion
 }
