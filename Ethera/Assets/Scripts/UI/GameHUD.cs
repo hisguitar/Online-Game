78a1b4ef3889b60d25a1cf6b4d7a9c56283cbc13
Guitar <wachsirin.sara@gmail.com>
2024-06-25 17:52:17 +0700

    Add free UI icon, Redesign 'GameHUD' in 'Game' scene

--- Ethera/Assets/Scripts/UI/GameHUD.cs
+++ Ethera/Assets/Scripts/UI/GameHUD.cs
@@ -1,17 +1,67 @@
 using TMPro;
 using Unity.Netcode;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameHUD : MonoBehaviour
 {
     [SerializeField] private TMP_Text joinCodeText;
 
+    [Header("Panel")]
+    [SerializeField] private GameObject _gameHUDPanel;
+    [SerializeField] private GameObject _menuPanel;
+    [SerializeField] private GameObject _optionsPanel;
+
+    [Header("Button")]
+    [SerializeField] private Button _menuButton;
+    [SerializeField] private Button _resumeButton;
+    [SerializeField] private Button _optionsButton;
+    [SerializeField] private Button _mainMenuButton;
+    [SerializeField] private Button _quitButton;
+
     private void Start()
     {
-        joinCodeText.text = HostSingleton.Instance.GameManager.JoinCode;
+        joinCodeText.text = "Code\n" + HostSingleton.Instance.GameManager.JoinCode;
     }
 
-    public void LeaveGame()
+    private void OnEnable()
+    {
+        _menuButton.onClick.AddListener(OnClick_Menu);
+        _resumeButton.onClick.AddListener(OnClick_Resume);
+        _optionsButton.onClick.AddListener(OnClick_Options);
+        _mainMenuButton.onClick.AddListener(OnClick_MainMenu);
+        _quitButton.onClick.AddListener(OnClick_QuitGame);
+    }
+
+    private void OnDisable()
+    {
+        _menuButton.onClick.RemoveListener(OnClick_Menu);
+        _resumeButton.onClick.RemoveListener(OnClick_Resume);
+        _optionsButton.onClick.RemoveListener(OnClick_Options);
+        _mainMenuButton.onClick.RemoveListener(OnClick_MainMenu);
+        _quitButton.onClick.RemoveListener(OnClick_QuitGame);
+    }
+
+    #region Button
+    private void OnClick_Menu()
+    {
+        _gameHUDPanel.SetActive(false);
+        _menuPanel.SetActive(true);
+    }
+
+    private void OnClick_Resume()
+    {
+        _gameHUDPanel.SetActive(true);
+        _menuPanel.SetActive(false);
+    }
+
+    private void OnClick_Options()
+    {
+        _menuPanel.SetActive(false);
+        _optionsPanel.SetActive(true);
+    }
+
+    private void OnClick_MainMenu()
     {
         if (NetworkManager.Singleton.IsHost)
         {
@@ -20,4 +70,10 @@ public class GameHUD : MonoBehaviour
 
         ClientSingleton.Instance.GameManager.Disconnect();
     }
+
+    private void OnClick_QuitGame()
+    {
+        Application.Quit();
+    }
+    #endregion
 }
