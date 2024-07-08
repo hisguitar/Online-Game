e2c8f35f5dbe0e8a18a60777ea3de3be2c5f4a57
Guitar <wachsirin.sara@gmail.com>
2024-07-08 22:22:12 +0700

    Re-design Leaderboard

--- Ethera/Assets/Scripts/UI/Leaderboard/LeaderboardEntityDisplay.cs
+++ Ethera/Assets/Scripts/UI/Leaderboard/LeaderboardEntityDisplay.cs
@@ -5,34 +5,40 @@ using UnityEngine;
 
 public class LeaderboardEntityDisplay : MonoBehaviour
 {
-    [SerializeField] private TMP_Text displayText;
-    [SerializeField] private Color myColor;
+	[SerializeField] private TMP_Text rankText;
+	[SerializeField] private TMP_Text nameText;
+	[SerializeField] private TMP_Text expText;
+	[SerializeField] private Color myColor;
 
-    private FixedString32Bytes playerName;
+	private FixedString32Bytes playerName;
 
-    public ulong ClientId { get; private set; }
-    public int Exp { get; private set; }
+	public ulong ClientId { get; private set; }
+	public int Exp { get; private set; }
 
-    public void Initialise(ulong clientId, FixedString32Bytes playerName, int exp)
-    {
-        ClientId = clientId;
-        this.playerName = playerName;
-        UpdateExp(exp);
+	public void Initialise(ulong clientId, FixedString32Bytes playerName, int exp)
+	{
+		ClientId = clientId;
+		this.playerName = playerName;
+		UpdateExp(exp);
 
-        if(clientId == NetworkManager.Singleton.LocalClientId)
-        {
-            displayText.color = myColor;
-        }
-    }
+		if(clientId == NetworkManager.Singleton.LocalClientId)
+		{
+			rankText.color = myColor;
+			nameText.color = myColor;
+			expText.color = myColor;
+		}
+	}
 
-    public void UpdateExp(int exp)
-    {
-        Exp = exp;
-        UpdateText();
-    }
+	public void UpdateExp(int exp)
+	{
+		Exp = exp;
+		UpdateText();
+	}
 
-    public void UpdateText()
-    {
-        displayText.text = $"({transform.GetSiblingIndex()+1}) {playerName} - {Exp}";
-    }
+	public void UpdateText()
+	{
+		rankText.text = $"{transform.GetSiblingIndex()+1}";
+		nameText.text = $"{playerName}";
+		expText.text = $"{Exp}";
+	}
 }
