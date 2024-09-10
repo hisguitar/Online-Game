e566be35184bd70df5a714be660556e7cc75c627
Guitar <wachsirin.sara@gmail.com>
2024-09-10 22:42:31 +0700

    Import icon for menu scene and re-design confirm setting panel

--- Ethera/Assets/Scripts/Settings/ConfirmSetting.cs
+++ Ethera/Assets/Scripts/Settings/ConfirmSetting.cs
@@ -5,81 +5,82 @@ using UnityEngine.UI;
 
 public class ConfirmSetting : MonoBehaviour
 {
-    [Header("Confirmation Window")]
-    [SerializeField] private GameObject _confirmPanel;
-    [SerializeField] private TMP_Text countdownText;
-    [SerializeField][Tooltip("Just put button to this blank, no need to attach function to button's OnClick()")] private Button confirmButton;
-    [SerializeField][Tooltip("Just put button to this blank, no need to attach function to button's OnClick()")] private Button revertButton;
+	[Header("Confirmation Window")]
+	[SerializeField] private GameObject _confirmPanel;
+	[SerializeField] private TMP_Text countdownText;
+	[SerializeField][Tooltip("Just put button to this blank, no need to attach function to button's OnClick()")] private Button confirmButton;
+	[SerializeField][Tooltip("Just put button to this blank, no need to attach function to button's OnClick()")] private Button revertButton;
+	[SerializeField] private string redColorCode = "#FF1A1A";
 
-    private Coroutine confirmationCoroutine;
-    private System.Action confirmAction;
-    private System.Action revertAction;
+	private Coroutine confirmationCoroutine;
+	private System.Action confirmAction;
+	private System.Action revertAction;
 
-    private void Start()
-    {
-        _confirmPanel.SetActive(false);
-    }
+	private void Start()
+	{
+		_confirmPanel.SetActive(false);
+	}
 
-    // OnEnable() will be called when the game object SetActive(true) / enabled = true
-    private void OnEnable()
-    {
-        //Register button
-        confirmButton.onClick.AddListener(Confirm);
-        revertButton.onClick.AddListener(Revert);
-    }
+	// OnEnable() will be called when the game object SetActive(true) / enabled = true
+	private void OnEnable()
+	{
+		//Register button
+		confirmButton.onClick.AddListener(Confirm);
+		revertButton.onClick.AddListener(Revert);
+	}
 
-    // OnDisable() will be called when the game object SetActive(false) or enabled = false
-    private void OnDisable()
-    {
-        confirmButton.onClick.RemoveListener(Confirm);
-        revertButton.onClick.RemoveListener(Revert);
-    }
+	// OnDisable() will be called when the game object SetActive(false) or enabled = false
+	private void OnDisable()
+	{
+		confirmButton.onClick.RemoveListener(Confirm);
+		revertButton.onClick.RemoveListener(Revert);
+	}
 
-    public void Confirmation(int countdownTime, string whatIsSetting, System.Action confirmAction, System.Action revertAction)
-    {
-        this.confirmAction = confirmAction;
-        this.revertAction = revertAction;
+	public void Confirmation(int countdownTime, string whatIsSetting, System.Action confirmAction, System.Action revertAction)
+	{
+		this.confirmAction = confirmAction;
+		this.revertAction = revertAction;
 
-        /// Open window & Start coroutine
-        _confirmPanel.SetActive(true);
+		/// Open window & Start coroutine
+		_confirmPanel.SetActive(true);
 
-        if (confirmationCoroutine != null) StopCoroutine(confirmationCoroutine);
-        confirmationCoroutine = StartCoroutine(WaitForConfirmation(countdownTime, whatIsSetting));
-        ///
-    }
+		if (confirmationCoroutine != null) StopCoroutine(confirmationCoroutine);
+		confirmationCoroutine = StartCoroutine(WaitForConfirmation(countdownTime, whatIsSetting));
+		///
+	}
 
-    private IEnumerator WaitForConfirmation(int countdownTime, string whatIsSetting)
-    {
-        int seconds = countdownTime;
-        while (seconds > 0)
-        {
-            countdownText.text = $"Would you like to keep these {whatIsSetting} settings?\nReverting to previous settings in <color=red>{seconds}</color> seconds.";
-            seconds--;
-            yield return new WaitForSeconds(1);
-        }
-        Revert();
-    }
+	private IEnumerator WaitForConfirmation(int countdownTime, string whatIsSetting)
+	{
+		int seconds = countdownTime;
+		while (seconds > 0)
+		{
+			countdownText.text = $"Would you like to keep these {whatIsSetting} settings?\nReverting to previous settings in <color={redColorCode}>{seconds}</color> seconds.";
+			seconds--;
+			yield return new WaitForSeconds(1);
+		}
+		Revert();
+	}
 
-    #region Confirm & Revert Button
-    public void Confirm()
-    {
-        confirmAction?.Invoke();
-        StopConfirmation();
-    }
+	#region Confirm & Revert Button
+	public void Confirm()
+	{
+		confirmAction?.Invoke();
+		StopConfirmation();
+	}
 
-    public void Revert()
-    {
-        revertAction?.Invoke();
-        StopConfirmation();
-    }
+	public void Revert()
+	{
+		revertAction?.Invoke();
+		StopConfirmation();
+	}
 
-    private void StopConfirmation()
-    {
-        /// Stop coroutine & Close window
-        if (confirmationCoroutine != null) StopCoroutine(confirmationCoroutine);
+	private void StopConfirmation()
+	{
+		/// Stop coroutine & Close window
+		if (confirmationCoroutine != null) StopCoroutine(confirmationCoroutine);
 
-        _confirmPanel.SetActive(false);
-        ///
-    }
-    #endregion
+		_confirmPanel.SetActive(false);
+		///
+	}
+	#endregion
 }
