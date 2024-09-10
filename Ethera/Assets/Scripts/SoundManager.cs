4be90a9dee99a5f80233bd392cb819a1ec69ff7f
Guitar <wachsirin.sara@gmail.com>
2024-09-11 01:26:59 +0700

    Modify SoundManager to be able to setting

--- Ethera/Assets/Scripts/SoundManager.cs
+++ Ethera/Assets/Scripts/SoundManager.cs
@@ -13,7 +13,7 @@ public class SoundManager : SingletonPersistent<SoundManager>
 		NewLevel
 	}
 
-	// For playing sound
+	// For setting sound
 	public void Play(SoundName soundName)
 	{
 		var sound = GetSound(soundName);
@@ -23,23 +23,33 @@ public class SoundManager : SingletonPersistent<SoundManager>
 			sound.audioSource.clip = sound.clip;
 			sound.audioSource.volume = sound.volume;
 			sound.audioSource.loop = sound.loop;
+			sound.audioSource.outputAudioMixerGroup = sound.audioMixerGroup;
 		}
 		sound.audioSource.Play();
 	}
+
+	private Sound GetSound(SoundName soundName)
+	{
+		return Array.Find(sounds, s => s.soundName == soundName);
+	}
+
+	public void Stop(SoundName soundName)
+	{
+		var sound = GetSound(soundName);
+		if (sound.audioSource != null)
+		{
+			sound.audioSource.Stop();
+		}
+	}
 	
-	public void Stop()
+	public void StopAll()
 	{
 		foreach (var sound in sounds)
 		{
-			if (sound.audioSource != null && sound.audioSource.isPlaying)
+			if (sound.audioSource != null)
 			{
 				sound.audioSource.Stop();
 			}
 		}
-	}
-	
-	private Sound GetSound(SoundName soundName)
-	{
-		return Array.Find(sounds, s => s.soundName == soundName);
 	}
 }
